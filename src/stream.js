466880e6295154b0649e928ea3ff1973f5df22c4
Alan Shaw <alan.shaw@protocol.ai>
2019-05-02 16:01:16 +0100

    test: add stream tests

--- src/stream.js
+++ src/stream.js
@@ -6,10 +6,12 @@ const log = require('debug')('it-mplex:stream')
 const pushable = require('it-pushable')
 const { InitiatorMessageTypes, ReceiverMessageTypes } = require('./message-types')
 
-module.exports = ({ id, name, send, onEnd, type = 'initiator' }) => {
+module.exports = ({ id, name, send, onEnd = (() => {}), type = 'initiator' }) => {
   const abortController = new AbortController()
   const resetController = new AbortController()
   const Types = type === 'initiator' ? InitiatorMessageTypes : ReceiverMessageTypes
+
+  name = String(name == null ? id : name)
 
   let sourceEnded = false
   let sinkEnded = false
@@ -17,29 +19,32 @@ module.exports = ({ id, name, send, onEnd, type = 'initiator' }) => {
 
   const onSourceEnd = err => {
     sourceEnded = true
-    if (err) endErr = err
+    log(`%s stream %s source end`, type, name, err)
+    if (err && !endErr) endErr = err
     if (sinkEnded) onEnd(endErr)
   }
 
   const onSinkEnd = err => {
     sinkEnded = true
-    if (err) endErr = err
+    log(`%s stream %s sink end`, type, name, err)
+    if (err && !endErr) endErr = err
     if (sourceEnded) onEnd(endErr)
   }
 
   const stream = {
-    close: () => stream.source.end(), // Close for reading
-    abort: err => { // Close for reading and writing (local error)
-      abortController.abort()
+    // Close for reading
+    close: () => stream.source.end(),
+    // Close for reading and writing (local error)
+    abort: err => {
+      // End the source with the passed error
       stream.source.end(err)
+      abortController.abort()
     },
-    reset: () => { // Close immediately for reading and writing (remote error)
-      resetController.abort()
-      stream.source.end(new Error('stream reset'))
-    },
+    // Close immediately for reading and writing (remote error)
+    reset: () => resetController.abort(),
     sink: async source => {
-      source = abortable(source, abortController.signal)
-      source = abortable(source, resetController.signal, { abortCode: 'ERR_REMOTE_RESET' })
+      source = abortable(source, abortController.signal, { abortMessage: 'stream aborted', abortCode: 'ERR_MPLEX_STREAM_ABORT' })
+      source = abortable(source, resetController.signal, { abortMessage: 'stream reset', abortCode: 'ERR_MPLEX_STREAM_RESET' })
 
       if (type === 'initiator') { // If initiator, open a new stream
         send({ id, type: Types.NEW_STREAM, data: name })
@@ -51,12 +56,14 @@ module.exports = ({ id, name, send, onEnd, type = 'initiator' }) => {
         }
       } catch (err) {
         // Send no more data if this stream was remotely reset
-        if (err.code === 'ERR_REMOTE_RESET') {
-          log(`%s stream %s reset`, type, name || id)
+        if (err.code === 'ERR_MPLEX_STREAM_RESET') {
+          log(`%s stream %s reset`, type, name)
         } else {
-          log('%s stream %s error', type, name || id, err)
+          log('%s stream %s error', type, name, err)
           send({ id, type: Types.RESET })
         }
+
+        stream.source.end(err)
         return onSinkEnd(err)
       }
 
